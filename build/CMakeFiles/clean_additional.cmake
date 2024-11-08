9b1206fded0ae8da3f861785162f25317636ad8b
Suleiman G. K. <suleimankareithi@yahoo.com>
2024-11-08 03:34:58 +0300

    Add files via upload

---
+++ build/CMakeFiles/clean_additional.cmake
@@ -0,0 +1,44 @@
+# Additional clean files
+cmake_minimum_required(VERSION 3.16)
+
+if("${CONFIG}" STREQUAL "" OR "${CONFIG}" STREQUAL "Debug")
+  file(REMOVE_RECURSE
+  "CMakeFiles\\dice_autogen.dir\\AutogenUsed.txt"
+  "CMakeFiles\\dice_autogen.dir\\AutogenUsed_Debug.txt"
+  "CMakeFiles\\dice_autogen.dir\\AutogenUsed_RelWithDebInfo.txt"
+  "CMakeFiles\\dice_autogen.dir\\AutogenUsed_Release.txt"
+  "CMakeFiles\\dice_autogen.dir\\ParseCache.txt"
+  "CMakeFiles\\dice_autogen.dir\\ParseCache_Debug.txt"
+  "CMakeFiles\\dice_autogen.dir\\ParseCache_RelWithDebInfo.txt"
+  "CMakeFiles\\dice_autogen.dir\\ParseCache_Release.txt"
+  "dice_autogen"
+  )
+endif()
+
+if("${CONFIG}" STREQUAL "" OR "${CONFIG}" STREQUAL "Release")
+  file(REMOVE_RECURSE
+  "CMakeFiles\\dice_autogen.dir\\AutogenUsed.txt"
+  "CMakeFiles\\dice_autogen.dir\\AutogenUsed_Debug.txt"
+  "CMakeFiles\\dice_autogen.dir\\AutogenUsed_RelWithDebInfo.txt"
+  "CMakeFiles\\dice_autogen.dir\\AutogenUsed_Release.txt"
+  "CMakeFiles\\dice_autogen.dir\\ParseCache.txt"
+  "CMakeFiles\\dice_autogen.dir\\ParseCache_Debug.txt"
+  "CMakeFiles\\dice_autogen.dir\\ParseCache_RelWithDebInfo.txt"
+  "CMakeFiles\\dice_autogen.dir\\ParseCache_Release.txt"
+  "dice_autogen"
+  )
+endif()
+
+if("${CONFIG}" STREQUAL "" OR "${CONFIG}" STREQUAL "RelWithDebInfo")
+  file(REMOVE_RECURSE
+  "CMakeFiles\\dice_autogen.dir\\AutogenUsed.txt"
+  "CMakeFiles\\dice_autogen.dir\\AutogenUsed_Debug.txt"
+  "CMakeFiles\\dice_autogen.dir\\AutogenUsed_RelWithDebInfo.txt"
+  "CMakeFiles\\dice_autogen.dir\\AutogenUsed_Release.txt"
+  "CMakeFiles\\dice_autogen.dir\\ParseCache.txt"
+  "CMakeFiles\\dice_autogen.dir\\ParseCache_Debug.txt"
+  "CMakeFiles\\dice_autogen.dir\\ParseCache_RelWithDebInfo.txt"
+  "CMakeFiles\\dice_autogen.dir\\ParseCache_Release.txt"
+  "dice_autogen"
+  )
+endif()
